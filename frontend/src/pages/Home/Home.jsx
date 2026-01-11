09b51df90fa5eeb7c158ecdb9256c20e4992fd12
Sameed Siddiqui <sameedsiddiqui9@gmail.com>
2026-01-11 16:38:10 +0500

    feat: create note card and dialouge box UI

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
+import AddEditNote from "./AddEditNote";
+import Modal from "react-modal";
 
 const Home = () => {
+  const [openAddEditModel, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
   return (
     <>
       <Navbar />
+      <div className="container mx-auto">
+        <div className="grid grid-cols-3 gap-4 mt-8">
+          <NoteCard
+            title={"Meeting on 7th April"}
+            date={"3rd Apr 2026"}
+            content={
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam quibusdam et dolores magnam amet alias magni fuga optio incidunt aperiam distinctio, veniam sunt praesentium, provident aut ad iure accusantium temporibus."
+            }
+            tags={"#Meeting"}
+            isPinned={true}
+            onEdit={() => {}}
+            onDelete={() => {}}
+            onPinNote={() => {}}
+          />
+        </div>
+      </div>
+
+      <button
+        className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+        onClick={() => {
+          setOpenAddEditModal({
+            isShown: true,
+            data: null,
+            type: "add",
+          });
+        }}>
+        <MdAdd className="text-[32px] text-white" />
+      </button>
+
+      <Modal
+        isOpen={openAddEditModel.isShown}
+        onRequestClose={() => {}}
+        style={{
+          overlay: {
+            backgroundColor: "rgba(0,0,0,0.2)",
+          },
+        }}
+        contentLabal=""
+        className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll"
+      >
+        <AddEditNote />
+      </Modal>
     </>
   );
 };
